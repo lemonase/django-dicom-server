6bee57a9cf784ecb48b32c77f7382d8cc804158c
James Dixon <notjamesdixon@gmail.com>
2022-06-01 07:36:01 -0400

    add hook for running pynetdicom when django starts

--- dcm_server/home/apps.py
+++ dcm_server/home/apps.py
@@ -1,6 +1,48 @@
 from django.apps import AppConfig
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
+    # custom hook for running code at django start
+    def ready(self):
+        pass
+        # pynetdicomsrv()
+
+
+# TODO: move this into a separate module, maybe a separate namespace
+def pynetdicomsrv():
+    from pynetdicom import AE, evt, AllStoragePresentationContexts, debug_logger
+
+    debug_logger()
+
+    # Implement a handler for evt.EVT_C_STORE
+
+    def handle_store(event):
+        """Handle a C-STORE request event."""
+        # Decode the C-STORE request's *Data Set* parameter to a pydicom Dataset
+        ds = event.dataset
+
+        # Add the File Meta Information
+        ds.file_meta = event.file_meta
+
+        # Save the dataset using the SOP Instance UID as the filename
+        ds.save_as(ds.SOPInstanceUID, write_like_original=False)
+
+        # Return a 'Success' status
+        return 0x0000
+
+    handlers = [(evt.EVT_C_STORE, handle_store)]
+
+    # Initialise the Application Entity
+    ae = AE()
+
+    # Support presentation contexts for all storage SOP Classes
+    ae.supported_contexts = AllStoragePresentationContexts
+
+    # Start listening for incoming association requests
+    ae.start_server(("127.0.0.1", 11112), evt_handlers=handlers)
