3f80da3a19040888abcc7ca15dacb0e104643129
James Dixon <notjamesdixon@gmail.com>
2022-06-14 21:37:18 -0400

    create DicomServerRunner class

--- dcm_server/home/apps.py
+++ dcm_server/home/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 import logging
 
+# from .models import DicomServer
+
 logger = logging.getLogger(__name__)
 
 
@@ -11,15 +13,38 @@ class HomeConfig(AppConfig):
     # custom hook for running code at django start
     def ready(self):
         print("Starting DICOM Store SCP server")
+        # dicom_server = DicomServerRunner()
+        # dicom_server.run_server()
         # logger.info("Starting DICOM Store SCP server")
         # pynetdicomsrv()
 
 
-# TODO: move this into a separate module, maybe a separate namespace
-def pynetdicomsrv():
-    from pynetdicom import AE, evt, AllStoragePresentationContexts, debug_logger
+class DicomServerRunner:
+    def __init__(self, server_name="DICOM C-STORE SCP Server",
+                 addr="127.0.0.1", port=11112, ae_title="STORESCP"):
+        self.server_name = server_name
+        self.addr = addr
+        self.port = port
+        self.ae_title = ae_title
 
-    debug_logger()
+    def run_server(self):
+        from pynetdicom import AE, evt, AllStoragePresentationContexts, debug_logger
+        handlers = [
+            (evt.EVT_C_ECHO, self.handle_echo),
+            (evt.EVT_C_STORE, self.handle_store)
+        ]
+
+        # Initialise the Application Entity
+        ae = AE()
+
+        # Support presentation contexts for all storage SOP Classes
+        ae.supported_contexts = AllStoragePresentationContexts
+
+        # Start listening for incoming association requests
+        ae.start_server((self.addr, self.port),
+                        block=False, evt_handlers=handlers)
+
+        print("DICOM Server has been started!!")
 
     def handle_echo(event):
         requestor = event.assoc.requestor
@@ -45,18 +70,3 @@ def pynetdicomsrv():
 
         # Return a 'Success' status
         return 0x0000
-
-    handlers = [
-        (evt.EVT_C_ECHO, handle_echo),
-        (evt.EVT_C_STORE, handle_store)
-    ]
-
-    # Initialise the Application Entity
-    ae = AE()
-
-    # Support presentation contexts for all storage SOP Classes
-    ae.supported_contexts = AllStoragePresentationContexts
-
-    # Start listening for incoming association requests
-    ae.start_server(("127.0.0.1", 11112), blocking=False,
-                    evt_handlers=handlers)
