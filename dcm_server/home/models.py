6b050616534a9250b8d851f8f4c9fe5e300241a7
James Dixon <notjamesdixon@gmail.com>
2022-07-09 13:59:40 -0400

    change verbose names of DicomServer fields. make hostname unique

--- dcm_server/home/models.py
+++ dcm_server/home/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 # Create your models here.
 
 
 class DicomServer(models.Model):
-    ae_title = models.CharField(unique=True, max_length=64)
+    hostname = models.CharField(
+        unique=True, max_length=128, verbose_name="Hostname (friendly name)")
+    ae_title = models.CharField(
+        unique=True, max_length=64, verbose_name="AE Title")
     port = models.PositiveIntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(65536)])
-    hostname = models.CharField(max_length=128)
+        validators=[MinValueValidator(1), MaxValueValidator(65536)], verbose_name="Port Number")
+
+    class Meta:
+        verbose_name = "DICOM Server"
+        verbose_name_plural = "DICOM Servers"
 
     def __str__(self):
         return self.hostname
