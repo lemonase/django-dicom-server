a1c08e046e2d3383d5ca66c850e0f44a6d0969ae
James Dixon <notjamesdixon@gmail.com>
2022-07-10 18:55:35 -0400

    fresh start

--- djicom_server/urls.py
+++ djicom_server/urls.py
@@ -1,4 +1,4 @@
-"""dcm_server URL Configuration
+"""djicom_server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- home/models.py
+++ home/models.py
@@ -2,16 +2,19 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
-
-
 class DicomServer(models.Model):
     hostname = models.CharField(
         unique=True, max_length=128, verbose_name="Hostname (friendly name)")
-    ae_title = models.CharField(
-        unique=True, max_length=64, verbose_name="AE Title")
+    ae_title = models.CharField(max_length=64, verbose_name="AE Title")
+    ip_address = models.GenericIPAddressField(
+        default="127.0.0.1",
+        help_text="IP Address of DICOM server (default is localhost 127.0.0.1)",
+        verbose_name="IP Address"
+    )
     port = models.PositiveIntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(65536)], verbose_name="Port Number")
+        validators=[MinValueValidator(1), MaxValueValidator(65536)],
+        verbose_name="Port Number"
+    )
 
     class Meta:
         verbose_name = "DICOM Server"
