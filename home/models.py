84773a091aa74d17cacbb9120969a501fe3b8090
James Dixon <notjamesdixon@gmail.com>
2022-07-10 20:28:00 -0400

    add output_directory field to DicomServer model

--- home/models.py
+++ home/models.py
@@ -13,20 +13,25 @@ class DicomServer(models.Model):
         max_length=64,
         default="ANY-SCP",
         verbose_name="AE Title",
-        help_text="An AE Title for the DICOM server. The default is ANY-SCP"
+        help_text="An AE Title for the DICOM server. The default is ANY-SCP."
     )
     ip_address = models.GenericIPAddressField(
         default="127.0.0.1",
         verbose_name="IP Address",
-        help_text="The IP Address that this DICOM server will listen on (default is localhost 127.0.0.1)"
+        help_text="The IP Address that this DICOM server will listen on (default is localhost 127.0.0.1)."
     )
     port = models.PositiveIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(65536)],
         default=11112,
         verbose_name="Port Number",
-        help_text="The port number that this DICOM Server will listen on (default is 11112)"
+        help_text="The port number that this DICOM Server will listen on (default is 11112)."
     )
     is_running = models.BooleanField(default=False)
+    output_directory = models.CharField(
+        max_length=255,
+        default="djicom_output/output",
+        help_text="The output directory where studies will be stored."
+    )
 
     class Meta:
         verbose_name = "DICOM Server"
